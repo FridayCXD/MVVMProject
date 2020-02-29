3299ecfff8287fc6acf44d2cb6adf3baecaa1c0f
xd <15711212720@163.com>
2020-02-29 11:39:17 +0800

    测试修改

--- MVVMModel/app/src/main/java/com/mvvm/model/MainActivity.kt
+++ MVVMModel/app/src/main/java/com/mvvm/model/MainActivity.kt
@@ -2,7 +2,9 @@ package com.mvvm.model
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+/**
+ * main activity
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
