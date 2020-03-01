6f1b06f3b9660cf7a70595acf2872f0d4d5fb087
xd <15711212720@163.com>
2020-03-01 20:25:58 +0800

    ViewPager2 使用

--- MVVMModel/app/src/main/java/com/mvvm/model/MainActivity.kt
+++ MVVMModel/app/src/main/java/com/mvvm/model/MainActivity.kt
@@ -2,10 +2,12 @@ package com.mvvm.model
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import com.mvvm.model.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * main activity
@@ -16,11 +18,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         setListener()
+        val adapter = ViewPager2Adapter()
+        val list = mutableListOf(1,2,3,4)
+        binding.viewPager2.adapter = adapter
+        adapter.setList(list)
+        binding.viewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
     }
 
     private fun setListener(){
-        findViewById<ImageButton>(R.id.backBtn).setOnClickListener{finish()}
+        findViewById<ImageView>(R.id.backBtn).setOnClickListener{finish()}
         findViewById<TextView>(R.id.title).text = "首页"
+        viewPager2?.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+            }
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+                println("$position , $positionOffset")
+            }
+
+        })
     }
 
 }
---
+++ MVVMModel/app/src/main/java/com/mvvm/model/ViewPager2Adapter.kt
@@ -0,0 +1,43 @@
+package com.mvvm.model
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @ClassName: ViewPager2Adapter
+ * @Description: 描述
+ * @Author: xd on  2020-03-01 19:06
+ */
+class ViewPager2Adapter: RecyclerView.Adapter<ViewPager2Adapter.PagerViewHolder>(){
+    private var list = emptyList<Int>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.adapter_view_pager_item, parent, false)
+        return PagerViewHolder(itemView = itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+        holder.bindData(position,list)
+    }
+
+    fun setList(list1: List<Int>){
+        this.list = list1
+        notifyDataSetChanged()
+    }
+
+    class PagerViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private val textView: TextView = itemView.findViewById(R.id.viewPager2Item)
+        private val colors = arrayOf("#CCFF99","#41F1E5","#8D41F1","#FF99CC")
+        fun bindData(position: Int, list: List<Int>) {
+            textView.text = list[position].toString()
+            textView.setBackgroundColor(Color.parseColor(colors[position]))
+        }
+    }
+}
