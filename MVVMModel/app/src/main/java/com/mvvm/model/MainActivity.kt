5d42fb4031e95e3563877d32010b793fc8c0cf8f
xd <15711212720@163.com>
2020-03-01 16:56:00 +0800

    add ViewPager2

--- MVVMModel/app/src/main/java/com/mvvm/model/MainActivity.kt
+++ MVVMModel/app/src/main/java/com/mvvm/model/MainActivity.kt
@@ -2,13 +2,25 @@ package com.mvvm.model
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.mvvm.model.databinding.ActivityMainBinding
+
 /**
  * main activity
  */
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setListener()
     }
+
+    private fun setListener(){
+        findViewById<ImageButton>(R.id.backBtn).setOnClickListener{finish()}
+        findViewById<TextView>(R.id.title).text = "首页"
+    }
+
 }
